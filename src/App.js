8f7eaa3f86f31a0c1a846538657cc65546a085a7
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-09-12 22:37:34 +0530

    Add Keygen page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Container, Navbar, Nav, NavbarBrand } from "react-bootstrap";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import KeyGenPage from './views/keygen';
+import NewCertPage from "./views/newcert";
 
 class App extends React.Component {
 
@@ -25,8 +27,8 @@ class App extends React.Component {
       <Router>
         <this.navbar></this.navbar>
         <Switch>
-          <Route path="/keygen"><h1>keygen</h1></Route>
-          <Route path="/newcert"><h1>newcert</h1></Route>
+          <Route path="/keygen"><KeyGenPage/></Route>
+          <Route path="/newcert"><NewCertPage/></Route>
           <Route path="/verifycert"><h1>verifycert</h1></Route>
           <Route path="/"><h1>home</h1></Route>
         </Switch>
---
+++ src/views/newcert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+
+class NewCertPage extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <Container>
+                <h2>Add a new file certificate to the BlockChain</h2>
+                
+                <form encType="multipart/form-data" action="">
+                    <input type="file" name="certFile"></input>
+                    <input type="submit" value="Upload"></input>
+                </form>
+            </Container>
+        );
+    }
+}
+
+export default NewCertPage;
